9a4c19319175eada12354f0805317ed962ca2d1f
Martin Lautenbacher <lautenbacher.martin@gmail.com>
2018-04-28 10:37:27 +0200

    focus now stays on original

--- rotate_layout.py
+++ rotate_layout.py
@@ -16,21 +16,29 @@ workspace = focused.workspace()
 leaves = workspace.leaves()
 
 def clock():
+    old_focus = 2
     for i in range(len(leaves)-1):
+        if leaves[i].id == focused.id:
+            old_focus = i
         comm = '[con_id=%s] swap container with con_id %s' % (str(leaves[i].id),
             str(leaves[i+1].id))
         i3.command(comm)
-    leaves[-1].command('focus')
+    leaves[old_focus - 1].command('focus')
 
 def counterclock():
+    old_focus = 0
     for i in range(len(leaves)-1, 0, -1):
+        if leaves[i].id == focused.id:
+            old_focus = i
         if i > 0:
             comm = '[con_id=%s] swap container with con_id %s' % (str(leaves[i].id),
                 str(leaves[i-1].id))
             i3.command(comm)
-    leaves[1].command('focus')
+    leaves[(old_focus + 1) % len(leaves)].command('focus')
 
 if args.direction == 0:
     clock()
 elif args.direction == 1:
     counterclock()
+
+#TODO keep focus on original
