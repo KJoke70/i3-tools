b5d87c4d8d4e45c893035792971fe39ac74f2d7a
Martin Lautenbacher <lautenbacher.martin@gmail.com>
2018-05-12 16:40:59 +0200

    with i3 commands

--- on_workspace_init.py
+++ on_workspace_init.py
@@ -15,6 +15,10 @@ parser = argparse.ArgumentParser(description='Execute a command upon ' \
         'creation of a workspace.')
 parser.add_argument('ws_cmd', help='define workspaces and commnands like ' \
         ' ws1,cmd1 ws2,cmd2,...', type=workspace_args, nargs='*')
+parser.add_argument('--no-exec', '-n', action='store_true', \
+        help='Disables automatic exec before command. Requires you to ' \
+        'write every command fully, e.g exec firefox. Also allows the usage ' \
+        'of i3 commands')
 
 args = parser.parse_args()
 workspaces, commands = zip(*args.ws_cmd)
@@ -23,7 +27,10 @@ def on_workspace_init(self, e):
     """ execute command if a specific workspace was created """
     if e.current and e.current.name in workspaces:
         index = workspaces.index(e.current.name)
-        i3.command('exec %s' % (commands[index]))
+        if args.no_exec:
+            i3.command('%s' % (commands[index]))
+        else:
+            i3.command('exec %s' % (commands[index]))
 
 i3 = i3ipc.Connection()
 i3.on('workspace::init', on_workspace_init)
