6cd371b4553450c74ce0221e9f421bf5d8d585a5
Martin Lautenbacher <lautenbacher.martin@gmail.com>
2019-06-19 13:30:04 +0200

    script to show scratchpad windows in dmenu and show the chosen one

---
+++ get_scratchpad_windows.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import i3ipc
+import subprocess
+
+
+i3 = i3ipc.Connection()
+
+windows = i3.get_tree().scratchpad().leaves()
+
+d = dict()
+
+for w in windows:
+    d["[%s] %s" % (w.window_instance, w.name)] = w.id
+
+keys = [x for x in d.keys()]
+
+s = ""
+for x in d.keys():
+    s += x + "\n"
+
+
+try:
+    ps = subprocess.Popen(("echo", s), stdout=subprocess.PIPE)
+    output = subprocess.check_output(("dmenu","-p", "Scratchpad:", "-l", "10"), stdin=ps.stdout)
+    ps.wait()
+
+    key = output.decode('utf-8').rstrip()
+    value = d[key]
+
+    i3.command("[con_id='%s'] scratchpad show; move position center" % value)
+except:
+    pass
+
+
